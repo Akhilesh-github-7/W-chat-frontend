078b3826ca7c763a3db7435a9aa0d6e1dd508605
Akhilesh-github-7 <akhileshmsskp@gmail.com>
2026-01-28 16:39:03 +0530

    Revert: URL-driven active chat navigation

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,20 @@ import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
-import ChatPage from "./pages/ChatPage";
+import Sidebar from './components/Sidebar';
+import ChatWindow from './components/ChatWindow';
+import ProfilePage from './pages/ProfilePage';
+import ChatPage from "./pages/ChatPage"; // Import the new ChatPage
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/chat" element={<ChatPage />} />
-      <Route path="/chat/:chatId" element={<ChatPage />} />
+      <Route
+        path="/chat"
+        element={<ChatPage />} // Render ChatPage here
+      />
       <Route path="*" element={<Navigate to="/login" />} />
     </Routes>
   );
